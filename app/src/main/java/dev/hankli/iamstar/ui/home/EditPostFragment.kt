5dc4e58270d83278902bb77531510cc9270e61db
Hank Li <HankLi0130@gmail.com>
2020-09-16 16:15:42 +0800

    Update Kotlin version to 1.4.10

--- app/src/main/java/dev/hankli/iamstar/ui/home/EditPostFragment.kt
+++ app/src/main/java/dev/hankli/iamstar/ui/home/EditPostFragment.kt
@@ -1,11 +1,13 @@
 package dev.hankli.iamstar.ui.home
 
+import android.app.Activity.RESULT_OK
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
 import dev.hankli.iamstar.R
 import dev.hankli.iamstar.utils.BaseFragment
+import dev.hankli.iamstar.utils.Consts.REQUEST_PICK_MEDIAS
 import kotlinx.android.synthetic.main.fragment_edit_post.*
 
 class EditPostFragment : BaseFragment(R.layout.fragment_edit_post) {
@@ -24,7 +26,11 @@ class EditPostFragment : BaseFragment(R.layout.fragment_edit_post) {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_PICK_MEDIAS) {
+            if (resultCode == RESULT_OK) {
 
+            }
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
