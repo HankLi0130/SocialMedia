e56fa3d7110fedfee94d9c6104785fc210b03b2e
Hank Li <HankLi0130@gmail.com>
2020-12-07 10:27:29 +0800

    Add update actions for display name, description, first name, last name, email, phone number.

--- app/src/main/java/dev/hankli/iamstar/ui/profile/EditProfileFragment.kt
+++ app/src/main/java/dev/hankli/iamstar/ui/profile/EditProfileFragment.kt
@@ -1,14 +1,24 @@
 package dev.hankli.iamstar.ui.profile
 
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.viewModels
 import com.bumptech.glide.Glide
 import dev.hankli.iamstar.R
 import dev.hankli.iamstar.utils.BaseArchFragment
+import dev.hankli.iamstar.utils.ext.isInternetConnected
 import kotlinx.android.synthetic.main.fragment_edit_profile.*
 
 class EditProfileFragment : BaseArchFragment<EditProfileViewModel>(R.layout.fragment_edit_profile) {
+
+    override val hasOptionsMenu: Boolean
+        get() = true
+
+    override val menuRes: Int
+        get() = R.menu.single_action_ok
+
     override val viewModel: EditProfileViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -37,5 +47,43 @@ class EditProfileFragment : BaseArchFragment<EditProfileViewModel>(R.layout.frag
 
             view_input_profile_sex.editText?.setText(profile.sex)
         }
+
+        view_input_profile_display_name.editText?.doOnTextChanged { text, _, _, _ ->
+            viewModel.onDisplayNameChanged(text)
+        }
+
+        view_input_profile_description.editText?.doOnTextChanged { text, _, _, _ ->
+            viewModel.onDescriptionChanged(text)
+        }
+
+        view_input_profile_first_name.editText?.doOnTextChanged { text, _, _, _ ->
+            viewModel.onFirstNameChanged(text)
+        }
+
+        view_input_profile_last_name.editText?.doOnTextChanged { text, _, _, _ ->
+            viewModel.onLastNameChanged(text)
+        }
+
+        view_input_profile_email.editText?.doOnTextChanged { text, _, _, _ ->
+            viewModel.onEmailChanged(text)
+        }
+
+        view_input_profile_phone_number.editText?.doOnTextChanged { text, _, _, _ ->
+            viewModel.onPhoneNumberChanged(text)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_ok -> submit()
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun submit(): Boolean {
+        if (requireContext().isInternetConnected()) {
+            viewModel.submit()
+        } else viewModel.showNoInternet()
+        return true
     }
 }
--- app/src/main/java/dev/hankli/iamstar/ui/profile/EditProfileViewModel.kt
+++ app/src/main/java/dev/hankli/iamstar/ui/profile/EditProfileViewModel.kt
@@ -14,6 +14,8 @@ import kotlinx.coroutines.withContext
 
 class EditProfileViewModel : BaseViewModel() {
 
+    private lateinit var profile: Profile
+
     private val _profileData = MutableLiveData<Profile>()
     val profileData: LiveData<Profile>
         get() = _profileData
@@ -28,7 +30,49 @@ class EditProfileViewModel : BaseViewModel() {
                     showError(R.string.error_unknown)
                     return@withContext
                 }
+                this@EditProfileViewModel.profile = profile
                 _profileData.value = profile
+            }
+        }
+    }
+
+    fun onDisplayNameChanged(text: CharSequence?) {
+        val displayName = if (text.isNullOrEmpty()) null else text.toString().trimEnd()
+        profile.displayName = displayName
+    }
+
+    fun onDescriptionChanged(text: CharSequence?) {
+        val description = if (text.isNullOrEmpty()) null else text.toString().trimEnd()
+        profile.description = description
+    }
+
+    fun onFirstNameChanged(text: CharSequence?) {
+        val firstName = if (text.isNullOrEmpty()) null else text.toString().trimEnd()
+        profile.firstName = firstName
+    }
+
+    fun onLastNameChanged(text: CharSequence?) {
+        val lastName = if (text.isNullOrEmpty()) null else text.toString().trimEnd()
+        profile.lastName = lastName
+    }
+
+    fun onEmailChanged(text: CharSequence?) {
+        val email = if (text.isNullOrEmpty()) null else text.toString().trimEnd()
+        profile.email = email
+    }
+
+    fun onPhoneNumberChanged(text: CharSequence?) {
+        val phoneNumber = if (text.isNullOrEmpty()) null else text.toString().trimEnd()
+        profile.phoneNumber = phoneNumber
+    }
+
+    fun submit() {
+        callProgress(true)
+        viewModelScope.launch(IO) {
+            ProfileManager.update(profile)
+            withContext(Main) {
+                callProgress(false)
+                popBack()
             }
         }
     }
