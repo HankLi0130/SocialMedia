fa80187889f6cf7e82178bdd4222a98a234797e1
Hank Li <HankLi0130@gmail.com>
2020-12-22 11:57:17 +0800

    Strict the video whose duration has to less than 1 min on media selection.

--- app/src/main/java/dev/hankli/iamstar/utils/media/MediaPicker.kt
+++ app/src/main/java/dev/hankli/iamstar/utils/media/MediaPicker.kt
@@ -1,12 +1,17 @@
 package dev.hankli.iamstar.utils.media
 
 import android.Manifest
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import androidx.fragment.app.Fragment
 import com.zhihu.matisse.Matisse
 import com.zhihu.matisse.MimeType
 import com.zhihu.matisse.engine.impl.GlideEngine
+import com.zhihu.matisse.filter.Filter
+import com.zhihu.matisse.internal.entity.IncapableCause
+import com.zhihu.matisse.internal.entity.Item
+import dev.hankli.iamstar.R
 
 val mediaPickerPermissions = arrayOf(
     Manifest.permission.CAMERA,
@@ -24,7 +29,9 @@ fun showImagePicker(fragment: Fragment, maxSelectable: Int, requestCode: Int) {
         .countable(true)
         .capture(false)
 //        .captureStrategy(CaptureStrategy(false, "${BuildConfig.APPLICATION_ID}.fileprovider"))
-//        .thumbnailScale(0.85f)
+        .thumbnailScale(0.85f)
+        .showSingleMediaType(true)
+        .autoHideToolbarOnSingleTap(true)
         .imageEngine(GlideEngine())
         .forResult(requestCode)
 }
@@ -33,10 +40,13 @@ fun showVideoPicker(fragment: Fragment, maxSelectable: Int, requestCode: Int) {
     Matisse.from(fragment)
         .choose(videoTypes)
         .maxSelectable(maxSelectable)
+        .addFilter(VideoDurationFilter())
         .countable(true)
         .capture(false)
 //        .captureStrategy(CaptureStrategy(false, "${BuildConfig.APPLICATION_ID}.fileprovider"))
-//        .thumbnailScale(0.85f)
+        .thumbnailScale(0.85f)
+        .showSingleMediaType(true)
+        .autoHideToolbarOnSingleTap(true)
         .imageEngine(GlideEngine())
         .forResult(requestCode)
 }
@@ -47,4 +57,21 @@ fun obtainResult(data: Intent?): List<Uri> {
 
 fun obtainPathResult(data: Intent?): List<String> {
     return data?.let { Matisse.obtainPathResult(it) } ?: emptyList()
+}
+
+class VideoDurationFilter(private val duration: Long = 60000) : Filter() {
+    override fun constraintTypes(): Set<MimeType> = videoTypes
+
+    override fun filter(context: Context, item: Item): IncapableCause? {
+        if (!needFiltering(context, item)) return null
+
+        return if (item.duration > duration) {
+            val seconds = duration / 1000
+            IncapableCause(
+                IncapableCause.TOAST,
+                context.getString(R.string.error_video_duration, seconds)
+            )
+        } else null
+    }
+
 }
