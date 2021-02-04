2eec65c2bfa7acfbc0274e180d93531004848947
Hank Li <HankLi0130@gmail.com>
2021-02-04 20:20:40 +0800

    Insert "Add Schedule" button

--- app/src/main/java/dev/hankli/iamstar/ui/schedule/ScheduleFragment.kt
+++ app/src/main/java/dev/hankli/iamstar/ui/schedule/ScheduleFragment.kt
@@ -1,21 +1,28 @@
 package dev.hankli.iamstar.ui.schedule
 
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.DividerItemDecoration.VERTICAL
 import dev.hankli.iamstar.R
+import dev.hankli.iamstar.firebase.AuthManager
 import dev.hankli.iamstar.utils.ArchFragment
 import kotlinx.android.synthetic.main.fragment_schedule.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class ScheduleFragment : ArchFragment<ScheduleViewModel>(R.layout.fragment_schedule) {
+class ScheduleFragment :
+    ArchFragment<ScheduleViewModel>(R.layout.fragment_schedule, R.menu.fragment_schedule) {
     override val viewModel: ScheduleViewModel by viewModel()
 
     private val scheduleAdapter = ScheduleAdapter()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // Role Permission
+        val writable = AuthManager.currentUserId == app.influencerId
+        setMenuVisibility(writable)
 
         view_schedule_list.apply {
             setHasFixedSize(true)
@@ -25,5 +32,19 @@ class ScheduleFragment : ArchFragment<ScheduleViewModel>(R.layout.fragment_sched
 
         scheduleAdapter.item = viewModel.getSchedules()
         scheduleAdapter.notifyDataSetChanged()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_add_schedule -> {
+                toEditScheduleFragment()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun toEditScheduleFragment() {
+
     }
 }
