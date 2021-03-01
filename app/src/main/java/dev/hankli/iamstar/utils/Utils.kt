b2e45a060435c96fb4b97a9862d2570be61064c7
Hank Li <HankLi0130@gmail.com>
2021-03-01 17:53:01 +0800

    Update queryByInfluencer

--- app/src/main/java/dev/hankli/iamstar/utils/Utils.kt
+++ app/src/main/java/dev/hankli/iamstar/utils/Utils.kt
@@ -1,9 +1,17 @@
 package dev.hankli.iamstar.utils
 
+import dev.hankli.iamstar.utils.ext.timeToZero
 import java.util.*
 
+fun getCal(): Calendar = Calendar.getInstance(TimeZone.getDefault())
+
 fun getClearedCal(): Calendar {
-    val cal = Calendar.getInstance(TimeZone.getDefault())
+    val cal = getCal()
     cal.clear()
     return cal
+}
+
+fun getToday(): Calendar {
+    val cal = getCal()
+    return cal.timeToZero()
 }
--- app/src/main/java/dev/hankli/iamstar/utils/ext/CalendarX.kt
+++ app/src/main/java/dev/hankli/iamstar/utils/ext/CalendarX.kt
@@ -5,3 +5,10 @@ import java.util.*
 fun Calendar.toDateString() = this.time.toDateString()
 
 fun Calendar.toTimeString() = this.time.toTimeString()
+
+fun Calendar.timeToZero(): Calendar {
+    this.set(Calendar.HOUR_OF_DAY, 0)
+    this.set(Calendar.MINUTE, 0)
+    this.set(Calendar.SECOND, 0)
+    return this
+}
